e126a53cdf6ab7f2443a64d0ff60397e40d242f9
evgjeniy <dashkevich2016@mail.ru>
2022-08-31 14:23:01 +0300

    WoodThrowing + Score

---
+++ Assets/Scripts/SliderController.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Slider))]
+public class SliderController : MonoBehaviour
+{
+    private enum MoveTo { Left = -1, None = 0, Right = 1 }
+
+    [SerializeField] private float _speed;
+    [SerializeField] private RectTransform _fillArea;
+    [SerializeField] private RectTransform _handle;
+
+    private Slider _slider;
+    private MoveTo _direction;
+
+    private void Start()
+    {
+        _slider = GetComponent<Slider>();
+        _direction = MoveTo.Right;
+    }
+
+    private void Update()
+    {
+        if (_slider.value <= _slider.minValue) _direction = MoveTo.Right;
+        if (_slider.value >= _slider.maxValue) _direction = MoveTo.Left;
+
+        _slider.value += _speed * Time.deltaTime * (int)_direction;
+    }
+
+    public void SetToStart()
+    {
+        _slider.value = _slider.minValue;
+        enabled = true;
+    }
+
+    public bool IsHandleOverWinningArea()
+    {
+        Vector3[] corners = new Vector3[4];
+        _handle.GetWorldCorners(corners);
+        float centerX = (corners[0].x + corners[3].x) / 2.0f;
+
+        _fillArea.GetWorldCorners(corners);
+        return centerX >= corners[0].x && centerX <= corners[3].x;
+    }
+}
