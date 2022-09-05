5f7c731d09e8a0573b4b76376bb5299721c70b02
vanikiopik <vanikiopik@gmail.com>
2022-09-05 17:46:16 +0300

    UPD: Make adative GUI, Rework text expansion

--- Assets/Scripts/UI scripts/TextExpansion.cs
+++ Assets/Scripts/UI scripts/TextExpansion.cs
@@ -1,62 +1,16 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class TextExpansion : MonoBehaviour
 {
-    RectTransform RectT;
-    [SerializeField]
-    private float _transformPower = 1.2f;   
-    [SerializeField]
-    private float _scaleTime = 1.3f;
+    private RectTransform _rect;
+    private float _time;
 
-    /* float Offset;
-     [SerializeField]
-     private float _time;
-     [SerializeField]
-     private float _amp;
-     [SerializeField]
-     private float _freq;
-    */
-    [SerializeField]
-    private float _speed;
-
-
-    void Start()
-    {
-        RectT = GetComponent<RectTransform>(); 
-    }
-
+    void Start() => _rect = GetComponent<RectTransform>(); 
 
     void Update()
     {
-
-        /*_time =+ Time.time / _speed;
-        Offset =_amp * Mathf.Sin(_time * _freq);
-        Offset = Mathf.Clamp01(Offset);
-        RectT.localScale = new Vector3(Offset, RectT.localScale.y);*/
-
-        //WORKING ONE, JUST ONE STRING!
-        //RectT.localScale = new Vector3(Mathf.PingPong(Time.time / _scaleTime, _transformPower), Mathf.PingPong(Time.time / _scaleTime, _transformPower));
-
-
-
-
-        if (RectT.localScale.x <= 0.54f)
-        {
-            Invoke("expand", 1f);
-        }
-        else if (RectT.localScale.x >= 0.9f)
-        {
-            Invoke("erase", 1f);
-        }
-    }
-    void expand()
-    {
-        RectT.localScale = new Vector3(Mathf.Lerp(RectT.localScale.x, 1f, Time.deltaTime * _speed), 1, 1);
-    }
-    void erase()
-    {
-        RectT.localScale = new Vector3(Mathf.Lerp(RectT.localScale.x, 0.5f, Time.deltaTime * _speed), 1, 1);
+        _time += Time.deltaTime;
+        float scaleValue = (Mathf.Sin(_time) + 3) / 4f;
+        _rect.localScale = new Vector3(scaleValue, scaleValue, 1);
     }
 }
