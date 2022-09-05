664c2087b731418f107597e7bc890eb8df2a4ba0
vanikiopik <vanikiopik@gmail.com>
2022-09-05 14:10:49 +0300

    UPD: New version of text expansion

--- Assets/Scripts/UI scripts/TextExpansion.cs
+++ Assets/Scripts/UI scripts/TextExpansion.cs
@@ -10,15 +10,16 @@ public class TextExpansion : MonoBehaviour
     [SerializeField]
     private float _scaleTime = 1.3f;
 
-   /* float Offset;
+    /* float Offset;
+     [SerializeField]
+     private float _time;
+     [SerializeField]
+     private float _amp;
+     [SerializeField]
+     private float _freq;
+    */
     [SerializeField]
-    private float _time;
-    [SerializeField]
-    private float _amp;
-    [SerializeField]
-    private float _freq;
-    [SerializeField]
-    private float _speed;*/
+    private float _speed;
 
 
     void Start()
@@ -35,6 +36,27 @@ public class TextExpansion : MonoBehaviour
         Offset = Mathf.Clamp01(Offset);
         RectT.localScale = new Vector3(Offset, RectT.localScale.y);*/
 
-        RectT.localScale = new Vector3(Mathf.PingPong(Time.time / _scaleTime, _transformPower), Mathf.PingPong(Time.time / _scaleTime, _transformPower));
+        //WORKING ONE, JUST ONE STRING!
+        //RectT.localScale = new Vector3(Mathf.PingPong(Time.time / _scaleTime, _transformPower), Mathf.PingPong(Time.time / _scaleTime, _transformPower));
+
+
+
+
+        if (RectT.localScale.x <= 0.54f)
+        {
+            Invoke("expand", 1f);
+        }
+        else if (RectT.localScale.x >= 0.9f)
+        {
+            Invoke("erase", 1f);
+        }
+    }
+    void expand()
+    {
+        RectT.localScale = new Vector3(Mathf.Lerp(RectT.localScale.x, 1f, Time.deltaTime * _speed), 1, 1);
+    }
+    void erase()
+    {
+        RectT.localScale = new Vector3(Mathf.Lerp(RectT.localScale.x, 0.5f, Time.deltaTime * _speed), 1, 1);
     }
 }
